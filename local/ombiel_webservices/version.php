95f2a1ef3b1c5a2324a924af6a73833f9e1ca8a1
Gordon Alexander <gordon.alexander@exlibrisgroup.com>
2016-10-17 17:08:55 +0100

    Add SSO support from local_mobile

--- local/ombiel_webservices/version.php
+++ local/ombiel_webservices/version.php
@@ -11,6 +11,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2016100701;   // The (date) version of this plugin
+$plugin->version  = 2016101701;   // The (date) version of this plugin
 $plugin->requires = 2015051100;   // Requires this Moodle version.
 $plugin->component = 'local_ombiel_webservices';  // Full name of the plugin (used for diagnostics)
