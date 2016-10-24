f1a408779ae30e06a9206549d83e41c773e26c6a
Gordon Alexander <gordon.alexander@exlibrisgroup.com>
2016-10-24 11:31:48 +0100

    Remove default settings from Alerts db install

--- message/output/ombiel_alerts/db/install.php
+++ message/output/ombiel_alerts/db/install.php
@@ -19,20 +19,20 @@ function xmldb_message_ombiel_alerts_install(){
     $provider = new stdClass();
     $provider->name  = 'ombiel_alerts';
     $DB->insert_record('message_processors', $provider);
-    
-    $defaults = (array) $DB->get_records_select('config_plugins', "plugin = 'message' AND name LIKE 'message_provider_%'");
-    
-    foreach($defaults as $defaultsetting) {
-        $defaultsetting->value .= ',ombiel_alerts';
-        $DB->update_record('config_plugins', $defaultsetting);
-    }
-    
-    $actuals = (array) $DB->get_records_select('user_preferences', "name LIKE 'message_provider_%'");
-    
-    foreach($actuals as $actualsetting) {
-        $actualsetting->value .= ',ombiel_alerts';
-        $DB->update_record('user_preferences', $actualsetting);
-    }
-    
+    // 
+    // $defaults = (array) $DB->get_records_select('config_plugins', "plugin = 'message' AND name LIKE 'message_provider_%'");
+    // 
+    // foreach($defaults as $defaultsetting) {
+    //     $defaultsetting->value .= ',ombiel_alerts';
+    //     $DB->update_record('config_plugins', $defaultsetting);
+    // }
+    // 
+    // $actuals = (array) $DB->get_records_select('user_preferences', "name LIKE 'message_provider_%'");
+    // 
+    // foreach($actuals as $actualsetting) {
+    //     $actualsetting->value .= ',ombiel_alerts';
+    //     $DB->update_record('user_preferences', $actualsetting);
+    // }
+    // 
     return $result;
 }
